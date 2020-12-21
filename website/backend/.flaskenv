1e33f2f4fb2de57681934485f085885ea8efb7cd
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2020-12-21 15:18:47 +0100

    Started integrating lat/lon into ML model. Add website skeleton

---
+++ website/backend/.flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=woher.py
+FLASK_ENV=development
---
+++ website/backend/app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+
+import time
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/time')
+def get_time():
+    return {"time": time.time()}
